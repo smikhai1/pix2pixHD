09a90cf39bee2d7f7234b907a34addf5db76ccf4
smikhai1 <sidorenko.mikhail94@gmail.com>
2023-07-27 21:53:07 +0300

    remove concats saving

--- options/train_options.py
+++ options/train_options.py
@@ -44,6 +44,6 @@ class TrainOptions(BaseOptions):
                                  help='Weight of the color preservation loss')
         self.parser.add_argument('--color_stats_loss_w', type=float, default=0.0,
                                  help='Weight of the color statistics preservation loss')
-        self.parser.add_argument('--not_save_concats', action='store_false', help='Whether not to save concats')
+        self.parser.add_argument('--not_save_concats', action='store_true', default=False, help='Whether not to save concats')
 
         self.isTrain = True
